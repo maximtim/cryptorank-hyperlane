b4f51f1d324ffab0a0753f485ddf9ad8706fa7d2
timofeev <maximtim1337@gmail.com>
2024-04-12 20:03:23 +0300

    fix bridge script

--- typescript/cli/src/send/crk721send.ts
+++ typescript/cli/src/send/crk721send.ts
@@ -1,15 +1,20 @@
+import * as dotenv from 'dotenv';
 import { Wallet } from 'ethers';
 
 import { MultiProvider } from '@hyperlane-xyz/sdk';
 import { addressToBytes32 } from '@hyperlane-xyz/utils';
 
+// import { addressToBytes32 } from '@hyperlane-xyz/utils';
 import { log } from '../../logger.js';
 import { testCrk721Configs } from '../deploy/cryptorank/erc721/config.js';
 import { cryptorankERC721Factories } from '../deploy/cryptorank/erc721/contracts.js';
 
+dotenv.config();
+
 async function main() {
   console.info('Getting signer');
-  const signer = new Wallet('key');
+  const key = process.env.HYP_KEY ?? '';
+  const signer = new Wallet(key);
 
   console.info('Preparing utilities');
   const multiProvider = new MultiProvider(testCrk721Configs);
@@ -25,12 +30,14 @@ async function main() {
   const mumbaiNetwork = await mumbaiSigner.provider!.getNetwork();
   const bsctestNetwork = await bsctestSigner.provider!.getNetwork();
 
+  console.info('Go');
+
   const bscContract = cryptorankERC721Factories.erc721
-    .attach('0xAfFe8C96f930Afcc407aD31C9F312BF7BC72a52f')
+    .attach('0x3577e4879DFcd45C5B17B7Af24cCd2CC7396a5f8')
     .connect(bsctestSigner);
 
   const mumContract = cryptorankERC721Factories.erc721
-    .attach('0x593Ac85a225400eBAAcE75207cb051963A9C76d7')
+    .attach('0x90f4AF35D2c8a33A2F6dF4BDd3bbDEeBaa017Cf8')
     .connect(mumbaiSigner);
 
   const destGas = await bscContract.destinationGas(mumbaiNetwork.chainId);
@@ -40,10 +47,10 @@ async function main() {
   // log(mailbox);
 
   const gas = await bscContract.quoteGasPayment(mumbaiNetwork.chainId);
-  log(gas.toNumber());
+  log(gas.toString());
 
   const gas2 = await mumContract.quoteGasPayment(bsctestNetwork.chainId);
-  log(gas2.toNumber());
+  log(gas2.toString());
 
   const balance = await mumContract.balanceOf(
     '0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB',
@@ -52,25 +59,25 @@ async function main() {
     '0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB',
     0,
   );
-  const token1 = await mumContract.tokenOfOwnerByIndex(
-    '0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB',
-    0,
-  );
-  log(balance.toNumber(), ' ', token0.toNumber(), ' ', token1.toNumber());
+  // const token1 = await mumContract.tokenOfOwnerByIndex(
+  //   '0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB',
+  //   1,
+  // );
+  log(balance.toNumber(), ' ', token0.toNumber());
 
-  const tx = await mumContract['transferRemote(uint32,bytes32,uint256,string)'](
-    bsctestNetwork.chainId,
+  const tx = await bscContract['transferRemote(uint32,bytes32,uint256,string)'](
+    mumbaiNetwork.chainId,
     addressToBytes32('0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB'),
     bsctestNetwork.chainId * 10 ** 7 + 1,
     'test bridge',
-    { value: destGas },
+    { value: gas },
   );
 
   await tx.wait();
   log(tx.hash);
 
-  const mintTx = await bscContract.mintBatch(3, 'test mint', { value: 3000 });
-  log(mintTx.hash);
+  // const mintTx = await bscContract.mintBatch(3, 'test mint', { value: 3000 });
+  // log(mintTx.hash);
 }
 
 main()
