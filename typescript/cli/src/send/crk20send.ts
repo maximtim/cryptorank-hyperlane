9fdb33cb71474d401150e068b76debd8c9299c91
timofeev <maximtim1337@gmail.com>
2024-06-06 01:16:04 +0300

    scripts fix

--- typescript/cli/src/send/crk20send.ts
+++ typescript/cli/src/send/crk20send.ts
@@ -1,11 +1,10 @@
 import * as dotenv from 'dotenv';
-import { Wallet } from 'ethers';
+import { Wallet, ethers } from 'ethers';
 
 import { MultiProvider, cryptorankFactories } from '@hyperlane-xyz/sdk';
-import { addressToBytes32 } from '@hyperlane-xyz/utils';
 
-import { chainCrk721Configs } from '../deploy/cryptorank/erc721/config.js';
 // import { addressToBytes32 } from '@hyperlane-xyz/utils';
+import { chainCrk20Configs } from '../deploy/cryptorank/erc20/config.js';
 import { log } from '../logger.js';
 
 dotenv.config();
@@ -21,27 +20,27 @@ async function main() {
   const signer = new Wallet(key);
 
   console.info('Preparing utilities');
-  const multiProvider = new MultiProvider(chainCrk721Configs);
+  const multiProvider = new MultiProvider(chainCrk20Configs);
   multiProvider.setSharedSigner(signer);
 
   // const core = HyperlaneCore.fromEnvironment('testnet', multiProvider);
   // const config = core.getRouterConfig(signer.address);
 
   // const router = cryptorankERC721Factories.erc721.
-  const bsctestSigner = multiProvider.getSigner('sepolia');
-  const mumbaiSigner = multiProvider.getSigner('bsctestnet');
+  const bsctestSigner = multiProvider.getSigner('bsc');
+  const mumbaiSigner = multiProvider.getSigner('inevm');
 
   const mumbaiNetwork = await mumbaiSigner.provider!.getNetwork();
   const bsctestNetwork = await bsctestSigner.provider!.getNetwork();
 
   console.info('Go');
 
-  const bscContract = cryptorankFactories.erc721
-    .attach('0x979723b0832341db34d329123F208E0f9e8E6c66')
+  const bscContract = cryptorankFactories.erc20
+    .attach('0xe4f10D31329d1c42961C4106FDcA8D7E12e5fA9e')
     .connect(bsctestSigner);
 
-  const mumContract = cryptorankFactories.erc721
-    .attach('0x8ff85Ec96fa44da00F9A80cd138A4E7d37F6ee12')
+  const mumContract = cryptorankFactories.erc20
+    .attach('0xe06D91fe2B448cBf7195709F2F7a25DfD8d33fDa')
     .connect(mumbaiSigner);
 
   const destGas = await bscContract.destinationGas(mumbaiNetwork.chainId);
@@ -56,29 +55,51 @@ async function main() {
   const gas2 = await mumContract.quoteGasPayment(bsctestNetwork.chainId);
   log(gas2.toString());
 
-  const balance = await mumContract.balanceOf(
+  const balance = await bscContract.balanceOf(
     '0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB',
-  );
-  const token0 = await mumContract.tokenOfOwnerByIndex(
-    '0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB',
-    0,
   );
   // const token1 = await mumContract.tokenOfOwnerByIndex(
   //   '0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB',
   //   1,
   // );
-  log(balance.toNumber().toString(), ' ', token0.toNumber());
+  log(ethers.utils.formatEther(balance));
 
-  const tx = await bscContract['transferRemote(uint32,bytes32,uint256,string)'](
-    mumbaiNetwork.chainId,
-    addressToBytes32('0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB'),
-    bsctestNetwork.chainId * 10 ** 7 + 1,
-    'test bridge',
-    { value: gas },
-  );
+  // const estimate = await mumContract.estimateGas[
+  //   'transferRemote(uint32,bytes32,uint256,string)'
+  // ](
+  //   bsctestNetwork.chainId,
+  //   addressToBytes32('0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB'),
+  //   balance.div(2),
+  //   'test bridge',
+  //   { value: gas2 },
+  // );
 
-  await tx.wait();
-  log(tx.hash);
+  // log(estimate.toNumber().toString());
+
+  // const hook = await mumContract.hook();
+  // console.log(hook);
+
+  // const owner = await mumContract.owner();
+  // console.log(owner);
+
+  // const txHook = await mumContract.setHook(
+  //   '0x19dc38aeae620380430C200a6E990D5Af5480117',
+  // );
+  // await txHook.wait();
+
+  // const hook2 = await mumContract.hook();
+  // console.log(hook2);
+
+  // const tx = await mumContract['transferRemote(uint32,bytes32,uint256,string)'](
+  //   bsctestNetwork.chainId,
+  //   addressToBytes32('0x069AfcBc2b655Ac8586F7fC6C60e45f4f4BBb6bB'),
+  //   balance.div(10),
+  //   'test bridge',
+  //   { value: gas2 }, // + 5%
+  // );
+
+  // await tx.wait();
+  // log(tx.hash);
 
   // const mintTx = await bscContract.mintBatch(3, 'test mint', { value: 3000 });
   // log(mintTx.hash);
--- typescript/cli/src/send/sethook.ts
+++ typescript/cli/src/send/sethook.ts
@@ -1,7 +1,11 @@
 import * as dotenv from 'dotenv';
 import { Wallet } from 'ethers';
 
-import { MultiProvider, cryptorankFactories } from '@hyperlane-xyz/sdk';
+import {
+  // CryptorankNFT__factory,
+  TokenRouter__factory,
+} from '@hyperlane-xyz/core';
+import { MultiProvider } from '@hyperlane-xyz/sdk';
 
 import { chainCrk20Configs } from '../deploy/cryptorank/erc20/config.js';
 
@@ -15,27 +19,37 @@ async function main() {
   console.info('Preparing utilities');
   const multiProvider = new MultiProvider(chainCrk20Configs);
   multiProvider.setSharedSigner(signer);
-  const inevmSigner = multiProvider.getSigner('inevm');
+  const inevmSigner = multiProvider.getSigner('arbitrum');
 
   console.info('Go');
 
-  const contract = cryptorankFactories.erc20
-    .attach('0xe06D91fe2B448cBf7195709F2F7a25DfD8d33fDa')
-    .connect(inevmSigner);
+  // const contract = cryptorankFactories.erc20
+  //   .attach('0xe06D91fe2B448cBf7195709F2F7a25DfD8d33fDa')
+  //   .connect(inevmSigner);
+
+  const contract = TokenRouter__factory.connect(
+    '0x1fcce39ccbe226ba69e277721d87516bee236e33',
+    inevmSigner,
+  );
 
   const hook = await contract.hook();
   console.log(hook);
 
+  // const x = contract.filters.ReferralBridge();
+
   const owner = await contract.owner();
   console.log(owner);
 
-  const txHook = await contract.setHook(
-    '0x19dc38aeae620380430C200a6E990D5Af5480117',
-  );
-  await txHook.wait();
+  // const txHook = await contract.setHook(
+  //   '0x19dc38aeae620380430C200a6E990D5Af5480117',
+  // );
+  // await txHook.wait();
 
-  const hook2 = await contract.hook();
-  console.log(hook2);
+  const destGas = await contract.destinationGas(169);
+  console.log(destGas.toNumber().toString());
+
+  // const hook2 = await contract.hook();
+  // console.log(hook2);
 }
 
 main()
