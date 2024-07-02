8209f790a296b218b732fe4e56e29cbc0327a582
maximtim <maximtim1337@gmail.com>
2024-07-03 02:15:43 +0300

    any tokens + ladder commissions

--- solidity/contracts/cryptorank/CryptorankNative.sol
+++ solidity/contracts/cryptorank/CryptorankNative.sol
@@ -3,40 +3,72 @@ pragma solidity >=0.8.0;
 
 import {TokenRouter} from "../token/libs/TokenRouter.sol";
 import {Address} from "@openzeppelin/contracts/utils/Address.sol";
+import {TokenMessage} from "../token/libs/TokenMessage.sol";
+import {TypeCasts} from "../libs/TypeCasts.sol";
+
+import {IERC20} from "@openzeppelin/contracts/token/ERC20/IERC20.sol";
+import {SafeERC20} from "@openzeppelin/contracts/token/ERC20/utils/SafeERC20.sol";
 
 contract CryptorankNative is TokenRouter {
-    error InsufficientPayment(uint256 fee, uint256 value);
-    error AmountExceeded(uint256 limit, uint256 amount);
+    using SafeERC20 for IERC20;
+
+    struct RouteConfig {
+        uint256 minAmount;
+        uint256 maxAmount;
+        uint256 minCommission;
+        uint256 maxCommission;
+        uint256 stepFirst;
+        uint256 stepCommission;
+        uint256 stepAmount;
+        uint256 decimalsUnit;
+    }
+    error NoTokenRoute();
+    error AmountTooLow();
+    error AmountTooHigh();
 
     event Deposit(address indexed sender, uint256 amount);
     event ReferralBridge(string referral, address sender, uint256 amount);
 
-    uint256 public constant DECIMALS_UNIT = 10 ** 18;
-
-    /// @notice Fee per 1 token (with decimals)
-    uint256 public bridgeFee;
-    uint256 public bridgeLimit;
+    mapping(address => mapping(uint32 => mapping(address => RouteConfig))) tokenRoutes;
 
     constructor(address _mailbox) TokenRouter(_mailbox) {}
 
-    function initialize(
-        address _owner,
-        uint256 _bridgeFee,
-        uint256 _bridgeLimit
-    ) public initializer {
+    function initialize(address _owner) public initializer {
         _MailboxClient_initialize(address(0), address(0), _owner);
-        bridgeFee = _bridgeFee;
-        bridgeLimit = _bridgeLimit;
+    }
+
+    function setRouteConfigs(
+        address[] calldata _tokensSrc,
+        address[] calldata _tokensDst,
+        uint32[] calldata _destinations,
+        RouteConfig[] calldata _routeConfigs
+    ) external onlyOwner {
+        uint256 length_ = _tokensSrc.length;
+        for (uint256 i = 0; i < length_; i++) {
+            tokenRoutes[_tokensSrc[i]][_destinations[i]][
+                _tokensDst[i]
+            ] = _routeConfigs[i];
+        }
     }
 
     function transferRemote(
         uint32 _destination,
+        address _tokenSrc,
+        address _tokenDst,
         bytes32 _recipient,
         uint256 _amount,
         string calldata _referral
     ) public payable virtual returns (bytes32 messageId) {
         emit ReferralBridge(_referral, msg.sender, _amount);
-        return _transferRemoteNative(_destination, _recipient, _amount);
+        return
+            _transferRemoteInternal(
+                _destination,
+                _tokenSrc,
+                _tokenDst,
+                _recipient,
+                _amount,
+                msg.value
+            );
     }
 
     function transferRemote(
@@ -44,39 +76,19 @@ contract CryptorankNative is TokenRouter {
         bytes32 _recipient,
         uint256 _amount
     ) public payable virtual override returns (bytes32 messageId) {
-        return _transferRemoteNative(_destination, _recipient, _amount);
-    }
-
-    function _transferRemoteNative(
-        uint32 _destination,
-        bytes32 _recipient,
-        uint256 _amount
-    ) private returns (bytes32 messageId) {
-        uint256 totalFee_ = (bridgeFee * _amount) / DECIMALS_UNIT;
-        uint256 required = _amount + totalFee_;
-
-        if (_amount > bridgeLimit) revert AmountExceeded(bridgeLimit, _amount);
-        if (msg.value < required)
-            revert InsufficientPayment(required, msg.value);
-
-        uint256 gasPayment = msg.value - required;
-
         return
-            _transferRemote(
+            _transferRemoteInternal(
                 _destination,
+                address(0),
+                address(0),
                 _recipient,
                 _amount,
-                gasPayment,
-                bytes(""),
-                address(0)
+                msg.value
             );
     }
 
-    function withdraw() public onlyOwner {
-        (bool success, ) = payable(msg.sender).call{
-            value: address(this).balance
-        }("");
-        require(success);
+    function withdraw(uint256 _amount, address _token) public onlyOwner {
+        _transferTo(msg.sender, _amount, _token);
     }
 
     function balanceOf(
@@ -103,9 +115,89 @@ contract CryptorankNative is TokenRouter {
     function _transferTo(
         address _recipient,
         uint256 _amount,
-        bytes calldata // no metadata
+        bytes calldata metadata
     ) internal virtual override {
-        Address.sendValue(payable(_recipient), _amount);
+        _transferTo(_recipient, _amount, address(uint160(bytes20(metadata))));
+    }
+
+    function _transferTo(
+        address _recipient,
+        uint256 _amount,
+        address _token
+    ) internal {
+        if (_token == address(0)) {
+            Address.sendValue(payable(_recipient), _amount);
+        } else {
+            IERC20(_token).safeTransfer(_recipient, _amount);
+        }
+    }
+
+    /**
+     * @notice Transfers `_amount` token to `_recipient` on `_destination` domain.
+     * @dev Delegates transfer logic to `_transferFromSender` implementation.
+     * @dev The metadata is the token metadata, and is DIFFERENT than the hook metadata.
+     * @dev Emits `SentTransferRemote` event on the origin chain.
+     * @param _destination The identifier of the destination chain.
+     * @param _recipient The address of the recipient on the destination chain.
+     * @param _amount The amount or identifier of tokens to be sent to the remote recipient.
+     * @param _msgValue The amount of native token to pay for interchain gas.
+     * @return messageId The identifier of the dispatched message.
+     */
+    function _transferRemoteInternal(
+        uint32 _destination,
+        address _tokenSrc,
+        address _tokenDst,
+        bytes32 _recipient,
+        uint256 _amount,
+        uint256 _msgValue
+    ) internal returns (bytes32 messageId) {
+        RouteConfig memory routeConfig_ = tokenRoutes[_tokenSrc][_destination][
+            _tokenDst
+        ];
+
+        if (routeConfig_.decimalsUnit == 0) revert NoTokenRoute();
+        if (_amount < routeConfig_.minAmount) revert AmountTooLow();
+        if (_amount > routeConfig_.maxAmount) revert AmountTooHigh();
+
+        uint256 commission_ = _calcCommission(_amount, routeConfig_);
+        uint256 nativeCommission = 0;
+
+        if (_tokenSrc == address(0)) {
+            nativeCommission = _amount + commission_;
+        } else {
+            IERC20(_tokenSrc).safeTransferFrom(
+                msg.sender,
+                address(this),
+                _amount + commission_
+            );
+        }
+
+        messageId = _dispatch(
+            _destination,
+            _msgValue - nativeCommission,
+            TokenMessage.format(
+                _recipient,
+                _amount,
+                abi.encodePacked(_tokenDst)
+            )
+        );
+
+        emit SentTransferRemote(_destination, _recipient, _amount);
+    }
+
+    function _calcCommission(
+        uint256 _amount,
+        RouteConfig memory _routeConfig
+    ) private pure returns (uint256 commission_) {
+        if (_amount < _routeConfig.stepFirst) return _routeConfig.minCommission;
+
+        commission_ =
+            ((_amount - _routeConfig.stepFirst) / _routeConfig.stepAmount + 1) *
+            _routeConfig.stepCommission +
+            _routeConfig.minCommission;
+
+        if (commission_ > _routeConfig.maxCommission)
+            return _routeConfig.maxCommission;
     }
 
     receive() external payable {
